df7e2ed55744ed4af601cc02ee5147bfb2f7307e
Alexander Tamoykin <at@zestcash.com>
2012-09-06 01:52:19 -0700

    Refactored cli_spec to change_spec

---
+++ spec/lib/simple-git-pair/command/change_spec.rb
@@ -0,0 +1,60 @@
+require 'simple-git-pair/command/change'
+
+describe SimpleGitPair::Command::Change do
+  before { Object.stub(:system) }
+  let(:opts) { [] }
+  let(:command) { described_class.new opts }
+
+  describe "#run!" do
+    subject { command.run! }
+
+    context "there is NO pairs file" do
+      before { SimpleGitPair::Helper.stub(:pairs_file_exists?).and_return false }
+      let(:opts) { ["some_initials"] }
+      it "complains and exit" do
+        SimpleGitPair::Helper.should_receive :complain_about_pairs_file
+        expect { subject }.to raise_error SystemExit
+      end
+    end
+
+    context "there is a pairs file" do
+      context "and multiple initials are passed in" do
+        before {
+          SimpleGitPair::Helper.stub(:pairs_file_exists?).and_return true
+          SimpleGitPair::Helper.stub(:names_for).and_return ["Super Mario", "Pac Man"]
+          command.stub! :system
+        }
+        let(:opts) { ['sm', 'pm'] }
+        it "changes only username to pair name" do
+          command.should_receive(:system).with("git config user.name 'Super Mario & Pac Man'")
+          subject
+        end
+      end
+
+      context "and single initial is passed in" do
+        before {
+          SimpleGitPair::Helper.stub(:pairs_file_exists?).and_return true
+          SimpleGitPair::Helper.stub(:names_for).and_return ["Pac Man"]
+          command.stub! :system
+        }
+        let(:opts) { ['pm'] }
+        it "changes only username to single name" do
+          command.should_receive(:system).with("git config user.name 'Pac Man'")
+          subject
+        end
+      end
+
+      context "and passed initial is unknown" do
+        let(:opts) { ['unknown_dude'] }
+        let(:ex)   { SimpleGitPair::Helper::NotFoundException.new "I don't know about #{opts.first}" }
+        before {
+          SimpleGitPair::Helper.stub(:pairs_file_exists?).and_return true
+          SimpleGitPair::Helper.stub(:names_for).and_raise ex
+          command.stub! :system
+          command.should_receive(:puts).with(ex.message)
+        }
+        it { expect { subject }.to raise_error SystemExit }
+      end
+    end
+  end
+end
