834f56a06dd2f3d7b623f34d3c0d5ee136d0cb96
Alexander Tamoykin <at@zestcash.com>
2012-09-06 15:06:27 -0700

    Dir.exists? -> File.exists? for compatibility with 1.8.7

--- features/support/env.rb
+++ features/support/env.rb
@@ -8,10 +8,10 @@ Before do
   @real_home = ENV['HOME']
   ENV['HOME'] = CUKE_TMP_DIR
   @pairs_file = File.join ENV['HOME'], ".git_pairs"
-  FileUtils.rm_rf CUKE_TMP_DIR if Dir.exists? CUKE_TMP_DIR
+  FileUtils.rm_rf CUKE_TMP_DIR if File.exists? CUKE_TMP_DIR
 end
 
 After do |scenario|
-  FileUtils.rm_rf CUKE_TMP_DIR if Dir.exists? CUKE_TMP_DIR
+  FileUtils.rm_rf CUKE_TMP_DIR if File.exists? CUKE_TMP_DIR
   ENV['HOME'] = @real_home
 end
