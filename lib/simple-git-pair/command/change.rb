ea73e77cc00b4355bce633ac24e7565c612ee47a
Alexander Tamoykin <at@zestcash.com>
2012-09-18 23:12:07 -0700

    Remove redundant code

--- lib/simple-git-pair/command/change.rb
+++ lib/simple-git-pair/command/change.rb
@@ -17,22 +17,6 @@ module SimpleGitPair
       end
 
       private
-
-      # Interactively creates a sample pairs file if pairs file doesn't exist
-      def ensure_pairs_file_exists
-        file_exists = Helper.pairs_file_exists?
-
-        unless file_exists
-          if agree("Can't find a config file. Create a sample one? (yes/no)")
-            file_exists = Helper.create_pairs_file
-          else
-            Helper.complain_about_pairs_file
-            file_exists = false
-          end
-        end
-
-        file_exists
-      end
     end
   end
 end
